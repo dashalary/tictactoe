f07db7e946621ff564ebe1b6648d7af9ae78523b
Dasha Lary <dasha4988@gmail.com>
2021-05-14 16:12:04 -0400

    declared functions

--- main.c
+++ main.c
@@ -2,6 +2,13 @@
 #include <stdlib.h>
 // #include <conio.h>
 
+char square[10] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
+int choice, player;
+
+int checkforWin();
+void displayBoard();
+void markBoard(char mark);
+
 int main()
 {
     return 0;
