9ef40f4e24ab2b974e436f25594008b34af3058b
Dasha Lary <dasha4988@gmail.com>
2021-05-14 16:25:22 -0400

    started writing second function

--- main.c
+++ main.c
@@ -13,3 +13,50 @@ int main()
 {
     return 0;
 }
+
+/* Function to return game status 1 for game is over with result, -1 for game is in progress, 0 for game is over and no result */
+
+int checkforWin()
+{
+    int returnValue = 0;
+
+    if (square[1] == square[2] && square[2] == square[3])
+    {
+        returnValue = 1;
+    }
+    else if (square[4] == square[5] && square[5] == square[6])
+        returnValue = 1;
+
+    else if (square[7] == square[8] && square[8] == square[9])
+        returnValue = 1;
+
+    else if (square[1] == square[4] && square[4] == square[7])
+        returnValue = 1;
+
+    else if (square[2] == square[5] && square[5] == square[8])
+        returnValue = 1;
+
+    else if (square[3] == square[6] && square[6] == square[9])
+        returnValue = 1;
+
+    else if (square[1] == square[5] && square[5] == square[9])
+        returnValue = 1;
+
+    else if (square[3] == square[5] && square[5] == square[7])
+        returnValue = 1;
+
+    else if (square[1] != '1' && square[2] != '2' && square[3] != '3' && square[4] != '4' && square[5] != '5' && square[6] != '6' && square[7] != '7' && square[8] != '8' && square[9] != '9')
+        returnValue = 0;
+
+    else
+        returnValue = -1;
+
+    return returnValue;
+}
+
+/* Function to draw board of Tic Tac Toe with players mark */
+
+void displayBoard()
+{
+    return 0;
+}
